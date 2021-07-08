a093d18290ace9f5328d0e8859ba088920d38385
mikedane <mikedane94@gmail.com>
2021-07-08 18:25:26 -0400

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script lang="ts" type="module">
+	import Phaser from 'phaser';
+
+	// console.log(window.navigator);
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
